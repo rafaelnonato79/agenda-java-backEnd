9fe004ce3280d35e641c6c253937895ea1951a4c
Rafael Nonato <rafaelnonato79@gmail.com>
2024-01-22 21:09:45 -0300

    adicionado botão de editar e excluir

--- src/main/java/br/senac/rn/agenda/webcontroller/HomeController.java
+++ src/main/java/br/senac/rn/agenda/webcontroller/HomeController.java
@@ -7,6 +7,7 @@ import br.senac.rn.agenda.service.ContatoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.model.IAttribute;
@@ -40,6 +41,20 @@ public class HomeController {
     @PostMapping("salvar")
     public String salvar(Contato contato){
         service.salvar(contato);
+        return "redirect:/";
+    }
+
+    @GetMapping("editar/{id}")
+    public String editar(@PathVariable Integer id, Model model){
+        Contato contato = service.listarPorId(id);
+        model.addAttribute("contato",contato);
+        model.addAttribute("tipos", TipoFone.values());
+        return "formulario";
+    }
+
+    @GetMapping("excluir/{id}")
+    public String excluir(@PathVariable Integer id){
+        service.excluir(id);
         return "redirect:/";
     }
 }
