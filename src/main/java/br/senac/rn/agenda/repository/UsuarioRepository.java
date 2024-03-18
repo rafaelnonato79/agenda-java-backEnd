9bf613cb1ba7de89157c6f807ee4775a35fd3bbc
Rafael Nonato <rafaelnonato79@gmail.com>
2024-03-18 19:19:50 -0300

    alterações feitas por Weskley

--- src/main/java/br/senac/rn/agenda/repository/UsuarioRepository.java
+++ src/main/java/br/senac/rn/agenda/repository/UsuarioRepository.java
@@ -1,14 +1,8 @@
 package br.senac.rn.agenda.repository;
 
-import br.senac.rn.agenda.model.Contato;
-import br.senac.rn.agenda.model.Fone;
 import br.senac.rn.agenda.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
